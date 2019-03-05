0fe76a1980dfcbfe176573ec5ab1d6b360f75b17
Sean Whitton <spwhitton@spwhitton.name>
2019-03-04 19:42:04 -0700

    notmuch-slurp-debbug: avoid invoking superfluous shells
    Reported-by: Paul Wise <pabs@debian.org>
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- mailscripts.el
+++ mailscripts.el
@@ -1,7 +1,7 @@
 ;;; mailscripts.el --- functions to access tools in the mailscripts package
 
 ;; Author: Sean Whitton <spwhitton@spwhitton.name>
-;; Version: 0.4
+;; Version: 0.5
 ;; Package-Requires: (notmuch)
 
 ;; Copyright (C) 2018 Sean Whitton
