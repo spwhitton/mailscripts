a565b2a000d7af91b305cff521f185f6deedd4ba
Sean Whitton <spwhitton@spwhitton.name>
2019-03-06 11:33:28 -0700

    notmuch-slurp-debbug: Fix the --bts-server argument
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- mailscripts.el
+++ mailscripts.el
@@ -1,7 +1,7 @@
 ;;; mailscripts.el --- functions to access tools in the mailscripts package
 
 ;; Author: Sean Whitton <spwhitton@spwhitton.name>
-;; Version: 0.6
+;; Version: 0.7
 ;; Package-Requires: (notmuch)
 
 ;; Copyright (C) 2018 Sean Whitton
