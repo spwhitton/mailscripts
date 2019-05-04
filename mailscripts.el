b0aa9fc4b5d6fffda94e68a8e21b337789d117fa
Sean Whitton <spwhitton@spwhitton.name>
2019-05-03 21:08:09 -0700

    mbox2maildir: acquire a lock, and flush after copying all messages
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- mailscripts.el
+++ mailscripts.el
@@ -1,7 +1,7 @@
 ;;; mailscripts.el --- functions to access tools in the mailscripts package
 
 ;; Author: Sean Whitton <spwhitton@spwhitton.name>
-;; Version: 0.7
+;; Version: 0.8
 ;; Package-Requires: (notmuch)
 
 ;; Copyright (C) 2018 Sean Whitton
