fdf112b63eb755fcaa5138567ed0428eea3957ff
Sean Whitton <spwhitton@spwhitton.name>
2019-11-02 09:17:44 -0700

    update version in mailscripts.el
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- mailscripts.el
+++ mailscripts.el
@@ -1,7 +1,7 @@
 ;;; mailscripts.el --- functions to access tools in the mailscripts package
 
 ;; Author: Sean Whitton <spwhitton@spwhitton.name>
-;; Version: 0.11
+;; Version: 0.12
 ;; Package-Requires: (notmuch)
 
 ;; Copyright (C) 2018 Sean Whitton
