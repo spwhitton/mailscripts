3c84e68d79ed84f916f1b983168d58e0f360686b
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2019-11-25 16:45:48 -0500

    email-print-mime-structure: decrypt S/MIME parts using gpgsm
    Decrypt ciphertext using gpgsm if the user has indicated that it's ok.
    
    This includes a new element in the test suite, which uses secret key
    material from https://www.ietf.org/id/draft-dkg-lamps-samples-01.html
    
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>
    Acked-by: Sean Whitton <spwhitton@spwhitton.name>

--- tests/email-print-mime-structure.sh
+++ tests/email-print-mime-structure.sh
@@ -11,13 +11,20 @@ test_eml() {
 for eml in tests/email-print-mime-structure/*.eml; do
     base="${eml%%.eml}"
     pgpkey="$base.pgpkey"
+    p12key="$base.p12"
     if [ -e "$pgpkey" ]; then
         printf "Testing %s (PGPy)\n" "${eml##*/}"
         test_eml "$base" --pgpkey "$pgpkey"
 
         testgpghome=$(mktemp -d)
-        printf "Testing %s (GnuPG)\n" "${eml##*/}"
+        printf "Testing %s (GnuPG PGP/MIME)\n" "${eml##*/}"
         gpg --homedir="$testgpghome" --batch --quiet --import <"$pgpkey"
+        GNUPGHOME="$testgpghome" test_eml "$base" --use-gpg-agent
+        rm -rf "$testgpghome"
+    elif [ -e "$p12key" ]; then
+        testgpghome=$(mktemp -d)
+        printf "Testing %s (GnuPG S/MIME)\n" "${eml##*/}"
+        gpgsm --pinentry-mode=loopback --passphrase-fd 4 4<<<'' --homedir="$testgpghome" --batch --quiet --import <"$p12key"
         GNUPGHOME="$testgpghome" test_eml "$base" --use-gpg-agent
         rm -rf "$testgpghome"
     else
