93d309e682c0910556d79aa75c8871cb3f6860ae
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2019-11-25 01:53:55 -0500

    email-print-mime-structure: added test for PGP/MIME decryption
    The secret key material in this test comes from
    https://datatracker.ietf.org/doc/draft-bre-openpgp-samples/
    
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>

--- tests/email-print-mime-structure.sh
+++ tests/email-print-mime-structure.sh
@@ -2,7 +2,20 @@
 
 set -e
 
+test_eml() {
+    message="$1"
+    shift
+    diff -u "$message.out" <(./email-print-mime-structure "$@" < "$message.eml")
+}
+
 for eml in tests/email-print-mime-structure/*.eml; do
-    printf "Testing %s\n" "${eml##*/}"
-    diff -u "${eml%%.eml}.out" <(./email-print-mime-structure < "$eml")
+    base="${eml%%.eml}"
+    pgpkey="$base.pgpkey"
+    if [ -e "$pgpkey" ]; then
+        printf "Testing %s (PGPy)\n" "${eml##*/}"
+        test_eml "$base" --pgpkey "$pgpkey"
+    else
+        printf "Testing %s\n" "${eml##*/}"
+        test_eml "$base"
+    fi
 done
