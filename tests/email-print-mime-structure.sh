b80164cb3c3e713a68c184096ec98293890a5ba9
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2019-11-25 01:53:56 -0500

    email-print-mime-structure: Add test for --use-gpg-agent as well
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>

--- tests/email-print-mime-structure.sh
+++ tests/email-print-mime-structure.sh
@@ -14,6 +14,12 @@ for eml in tests/email-print-mime-structure/*.eml; do
     if [ -e "$pgpkey" ]; then
         printf "Testing %s (PGPy)\n" "${eml##*/}"
         test_eml "$base" --pgpkey "$pgpkey"
+
+        testgpghome=$(mktemp -d)
+        printf "Testing %s (GnuPG)\n" "${eml##*/}"
+        gpg --homedir="$testgpghome" --batch --quiet --import < "$pgpkey"
+        GNUPGHOME="$testgpghome" test_eml "$base" --use-gpg-agent
+        rm -rf "$testgpghome"
     else
         printf "Testing %s\n" "${eml##*/}"
         test_eml "$base"
