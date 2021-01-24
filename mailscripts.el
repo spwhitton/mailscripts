bd274a20b46202f3a7d53a51a117cfd4110c3a67
Sean Whitton <spwhitton@spwhitton.name>
2021-01-23 17:18:10 -0700

    new script: gmi2email
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- mailscripts.el
+++ mailscripts.el
@@ -1,7 +1,7 @@
 ;;; mailscripts.el --- functions to access tools in the mailscripts package
 
 ;; Author: Sean Whitton <spwhitton@spwhitton.name>
-;; Version: 0.22
+;; Version: 0.23
 ;; Package-Requires: (notmuch)
 
 ;; Copyright (C) 2018, 2019, 2020 Sean Whitton
